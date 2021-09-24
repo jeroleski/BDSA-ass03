6bf1813df2661013ef74efc014db85a196c7e76d
nissemand243 <xander.s234@gmail.com>
2021-09-24 13:46:57 +0200

    tested and implemented IsSecure method

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,13 +1,22 @@
 using System;
 using Xunit;
 
-namespace BDSA2020.Assignment02.Tests
+namespace BDSA2020.Assignment3.Tests
 {
     public class ExtensionsTests
     {
         [Fact]
-        public void Test1()
+        public void IsSecure_Given_HTTP_Return_False()
         {
+            //Arrange
+            Uri input = new Uri("http://github.com/nissemand243/Assignment_01/blob/main/Assignment1.Tests/IteratorsTests.cs");
+
+            //Act
+            bool output = input.IsSecure();
+
+            //Assert
+
+            Assert.False(output);
 
         }
     }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,21 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace BDSA2020.Assignment02
+namespace BDSA2020.Assignment3
 {
     public static class Extensions
     {
+        public static bool IsSecure(this Uri input)
+        {
+            if(input.Scheme == Uri.UriSchemeHttps)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
