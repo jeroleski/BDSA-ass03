94f5dd353850832ca4535657cbd524f20f4bcc57
nissemand243 <47781194+nissemand243@users.noreply.github.com>
2021-09-24 14:00:28 +0200

    Alex (#2)
    * tested and implemented IsSecure method
    
    * Tested and implemented WordCount extension method

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,14 +1,39 @@
 using System;
 using Xunit;
 
-namespace BDSA2020.Assignment02.Tests
+namespace BDSA2020.Assignment3.Tests
 {
     public class ExtensionsTests
     {
         [Fact]
-        public void Test1()
+        public void IsSecure_Given_HTTP_Return_False()
         {
+            //Arrange
+            Uri input = new Uri("http://github.com/nissemand243/Assignment_01/blob/main/Assignment1.Tests/IteratorsTests.cs");
 
+            //Act
+            bool output = input.IsSecure();
+
+            //Assert
+
+            Assert.False(output);
+
+        }
+
+        [Fact]
+        public void WordCount_Given_HejMedDig_Return_3()
+        {
+            //Arrange
+            var input = "hej med dig";
+
+            var expected = 3;
+
+
+            //Act
+            var actual = input.WordCount();
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,23 @@
 using System;
 
-namespace BDSA2020.Assignment02
+namespace BDSA2020.Assignment3
 {
     public static class Extensions
     {
+        public static bool IsSecure(this Uri input)
+        {
+            if(input.Scheme == Uri.UriSchemeHttps)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static int WordCount(this string word)
+        {
+            return word.Split(" ").Length;
+        }
     }
 }
